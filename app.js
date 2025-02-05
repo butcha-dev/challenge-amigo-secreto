d1b410ad4642024a30688c20a33828650ff66949
Jose Zamora <jecszaco@gmail.com>
2025-02-05 13:34:49 -0500

    Usuario ingresa nombre al campo, y se añade a la lista amigos, si esta vacio se valida mediante un alert

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//  Inicializar array amigos.
+let amigos = [];
+
+//  Función para agregar amigos.
+function agregarAmigo(){
+
+    //  Capturando valor de entrada
+    let ingresoUsuario = document.getElementById("amigo").value;
+    console.log(ingresoUsuario);
+
+    if (ingresoUsuario == ""){
+        alert('Por favor, ingrese un nombre.');
+        return;
+    }
+    
+    amigos.push(ingresoUsuario)
+    document.getElementById("amigo").value = "";
+    
+    console.log(amigos)
+
+}
